a6cae75b661af114f21b992c1004ba57e1bec701
opencert4scdf <72531920+opencert4scdf@users.noreply.github.com>
2020-10-13 19:48:50 +0800

    Update index.tsx

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,5 +1,5 @@
 import { TemplateRegistry } from "@govtechsg/decentralized-renderer-react-components";
-import { templates } from "./coc";
+import { templates } from "./customTemplate";
 
 export const registry: TemplateRegistry<any> = {
   customTemplate: templates
